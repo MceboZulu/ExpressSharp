6f290497c0550ff5dcf55abbd4353059d12acc33
Mcebo Sbonelo Zulu <Mcebo@copalcor.ho.co.za>
2025-06-10 07:48:57 +0200

    Company/Organization

--- obj/Release/net9.0/ExpressSharp.AssemblyInfo.cs
+++ obj/Release/net9.0/ExpressSharp.AssemblyInfo.cs
@@ -10,8 +10,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Mcebo-Zulu")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Mcebo_Zulu")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Express.js-style fluent API for building minimal ASP.NET Core web apps.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64532edd7c34ba4a9ffb272c6c2e5f7be7d6addf")]
